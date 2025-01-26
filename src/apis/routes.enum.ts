d1d01347c8e0b86e72e8851355ec6eb4262288e1
terrance456 <terranceprasath97@gmail.com>
2025-01-26 19:27:44 +0800

    feat: 🎸 add expenses view

--- src/apis/routes.enum.ts
+++ src/apis/routes.enum.ts
@@ -6,4 +6,5 @@ export enum ApiRoutes {
   getMonthDebts = ApiBasePath.basePath + "/monthly-debt",
   addMonthlyDebt = ApiBasePath.basePath + "/add-monthly-debt",
   updatePosition = ApiBasePath.basePath + "/update-position",
+  deleteDebt = ApiBasePath.basePath + "/remove-monthly-debt",
 }
