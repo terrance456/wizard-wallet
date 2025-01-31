6feeeb2664b9621adf966bb2c4c5b3d768c836b1
terrance456 <terranceprasath97@gmail.com>
2025-01-31 18:28:58 +0800

    refactor: 💡 add dynamic route

--- app/api/reset-paid-status/route.ts
+++ app/api/reset-paid-status/route.ts
@@ -1,6 +1,8 @@
 import prisma from "@/lib/prisma";
 import type { NextRequest } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: NextRequest) {
   const authHeader = request.headers.get("authorization");
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
