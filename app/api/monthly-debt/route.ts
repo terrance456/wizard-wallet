433228454ec80ab7fadf026b10acfe6ca0a38265
terrance456 <terranceprasath97@gmail.com>
2025-01-31 22:31:48 +0800

    refactor: 💡 add serverTime in response

--- app/api/monthly-debt/route.ts
+++ app/api/monthly-debt/route.ts
@@ -5,7 +5,7 @@ import { User } from "next-auth";
 export async function GET() {
   try {
     const currentUser = (await auth())?.user as User;
-    return Response.json(await prisma.monthlyDebt.findMany({ where: { userId: currentUser.userId } }));
+    return Response.json({ ...(await prisma.monthlyDebt.findMany({ where: { userId: currentUser.userId } })), serverTime: new Date().toString() });
   } catch {
     return Response.json({ message: "Failed to retrive list, please try again" }, { status: 500 });
   }
